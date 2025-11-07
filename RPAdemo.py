848258c0afb4a746d17c58dd016107496bc6451a
priyalpc <110640581+priyalpc@users.noreply.github.com>
2025-11-07 16:07:51 +0530

    Add files via upload

---
+++ RPAdemo.py
@@ -0,0 +1,34 @@
+import pyautogui
+import time
+
+      
+   
+#mouse operations
+
+#pyautogui.click(100,100)
+#time.sleep(2)
+
+#pyautogui.rightClick(100,100)
+#time.sleep(5)
+
+#pyautogui.doubleClick(100,100)
+
+time.sleep(5)
+           
+#pyautogui.drag(100,100,200,200)
+
+#pyautogui.scroll(-500)
+time.sleep(1)
+
+#keyboard operations
+
+#pyautogui.write("hello priya")
+#time.sleep(2)
+
+
+
+pyautogui.hotkey('ctrl','a')
+time.sleep(3)
+
+pyautogui.press("enter")
+time.sleep(3)
---
+++ mousepointerfinder.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+x,y=pyautogui.position()
+
+print(f'x' :{x}, y :{y}')
+      
+      
